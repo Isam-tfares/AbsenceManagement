3a331760142fbe5c5e8fe69a1e45492d5f840283
abdelhamid <abdelhamidchebel25@gmail.com>
2024-05-12 15:38:06 +0100

    Connecting Our DB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const connectToDb = require("./Config/dbConnection");
 
 const app = express();
 
+connectToDb();
 
-PORT = process.env.PORT || 3000 ;
 
 app.use('/teacher',require("./Route/teacherRouter"));
 app.use('/student',require("./Route/studentRouter"));
 app.use('/module',require("./Route/moduleRouter"));
 // app.use('/admin',adminRouter);
+
+
+const PORT = process.env.PORT || 3000 ;
 
 app.listen(PORT, () => {
   console.log(
